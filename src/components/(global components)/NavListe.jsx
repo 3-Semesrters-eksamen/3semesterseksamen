7ec26432e56c60b114d620ee79d46cc8ff846204
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-07 14:04:01 +0200

    Navigation opsætning start

---
+++ src/components/(global components)/NavListe.jsx
@@ -0,0 +1,16 @@
+const NavListe = () => {
+  return (
+    <div className="nav-liste">
+      <ul>
+        <li>
+          <a href="#home">HOME</a>
+          <a href="#home">EVENTS</a>
+          <a href="#home">BOOK TABLE</a>
+          <a href="#home">CONTACT US</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavListe;
