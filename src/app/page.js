d99564eadc5d30961de291953c1005b22d20601a
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-08 10:10:41 +0200

    forside rettelse

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 import HeaderContainer from "../components/(ForsideComponents)/WelcomeClub";
 import Navigation from "../components/(globalComponents)/Navigation";
 import WelcomeClub from "../components/(ForsideComponents)/WelcomeClub";
+import FeaturedEventsContainer from "@/components/(ForsideComponents)/FeaturedEventsContainer";
+import ClubGallery from "@/components/(ForsideComponents)/ClubGallery";
+import ClubTrack from "@/components/(ForsideComponents)/ClubTrack";
+import LatestVideo from "@/components/(ForsideComponents)/LatestVideo";
+import NewsSubsriberContainer from "@/components/(ForsideComponents)/NewsSubscribeContainer";
 
 export default function Home() {
   return (
@@ -13,6 +18,14 @@ export default function Home() {
 
       <div className="main">
         <WelcomeClub />
+        <FeaturedEventsContainer />
+        <ClubGallery />
+        <ClubTrack />
+        <LatestVideo />
+        <NewsSubsriberContainer />
+      </div>
+      <div className="footer">
+        <footer />
       </div>
     </div>
   );
