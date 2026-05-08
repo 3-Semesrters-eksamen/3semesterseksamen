cc0cdcaf3753565932da719ed9cde92d1e77c989
roan0001 <roan0001@stud.kea.dk>
2026-05-08 10:57:52 +0200

    SmallLogo er sat op som et component

--- src/components/(footer components)/Footerinfo.jsx
+++ src/components/(footer components)/Footerinfo.jsx
@@ -1,19 +1,9 @@
+import SmallLogo from "../(globalComponents)/SamllLogo";
 export default function FooterInfo() {
   return (
     <>
       {/* Logo */}
-      <h1
-        style={{
-          fontFamily: "'Bebas Neue', sans-serif",
-          fontSize: "2.8rem",
-          letterSpacing: "0.06em",
-          lineHeight: 1,
-          margin: "0 0 6px",
-        }}
-      >
-        <span style={{ color: "#ffffff" }}>NIGHT</span>
-        <span style={{ color: "#e8004d" }}>CLUB</span>
-      </h1>
+      <SmallLogo />
 
       <p
         style={{
---
+++ src/components/(globalComponents)/SamllLogo.jsx
@@ -0,0 +1,16 @@
+export default function SmallLogo() {
+  return (
+    <h1
+      style={{
+        fontFamily: "'Bebas Neue', sans-serif",
+        fontSize: "2.8rem",
+        letterSpacing: "0.06em",
+        lineHeight: 1,
+        margin: "0 0 6px",
+      }}
+    >
+      <span style={{ color: "#ffffff" }}>NIGHT</span>
+      <span style={{ color: "#e8004d" }}>CLUB</span>
+    </h1>
+  );
+}
