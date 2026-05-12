1c3394f0547ac269505575440ab9a266a80ac0c8
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-12 22:58:37 +0200

    Navigation fiks Farver menu til oklch og smallLogo fiks

--- src/components/(globalComponents)/SmallLogo.jsx
+++ src/components/(globalComponents)/SmallLogo.jsx
@@ -1,31 +1,30 @@
 export default function SmallLogo() {
   return (
-    <>
+    <div className=" inline-block ">
       <h1
-        style={{
-          fontFamily: "'Bebas Neue', sans-serif",
-          fontSize: "2.8rem",
-          letterSpacing: "0.06em",
-          lineHeight: 1,
-          margin: "0 0 6px",
-        }}
+        className="font-bebas 
+        text-2xl sm:text-3xl   
+        tracking-[0.04em] sm:tracking-[0.06em]
+        leading-none 
+        mb-[4px] sm:mb-[6px]"
       >
-        <span style={{ color: "#ffffff" }}>NIGHT</span>
-        <span style={{ color: "#e8004d" }}>CLUB</span>
+        <span className="text-white">NIGHT</span>
+        <span className="text-[oklch(65%_0.23_370)]">CLUB</span>
       </h1>
 
       <p
-        style={{
-          fontSize: "0.6rem",
-          letterSpacing: "0.4em",
-          color: "rgba(255,255,255,0.38)",
-          textTransform: "uppercase",
-          fontWeight: 600,
-          margin: "0 0 36px",
-        }}
+        className=" text-white 
+        text-xs sm:text-sm     
+        uppercase 
+        font-regular 
+        whitespace-nowrap 
+        inline-block 
+        mx-auto 
+        [word-spacing:0.2em] sm:[word-spacing:0.3em]
+        [letter-spacing:0.1em] sm:[letter-spacing:0.1em]"
       >
         Have a good time
       </p>
-    </>
+    </div>
   );
 }
