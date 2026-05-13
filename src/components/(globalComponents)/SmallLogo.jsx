c114f76dc096ecf33e3e45dd4de32b2f13db9173
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-13 09:53:05 +0200

    smallLogo rettelse

--- src/components/(globalComponents)/SmallLogo.jsx
+++ src/components/(globalComponents)/SmallLogo.jsx
@@ -1,34 +1,5 @@
 export default function SmallLogo() {
   return (
-    <>
-      <div>
-        <h1
-          style={{
-            fontFamily: "'Bebas Neue', sans-serif",
-            fontSize: "3rem",
-            letterSpacing: "0.1em",
-            lineHeight: 1,
-            margin: "0 0 6px",
-          }}
-        >
-          <span style={{ color: "#ffffff" }}>NIGHT</span>
-          <span style={{ color: "#e8004d" }}>CLUB</span>
-        </h1>
-
-        <p
-          style={{
-            fontSize: "0.7rem",
-            letterSpacing: "0.6em",
-            color: "white",
-            textTransform: "uppercase",
-            fontWeight: 600,
-            margin: "0 0 36px",
-          }}
-        >
-          Have a good time
-        </p>
-      </div>
-    </>
     <div className=" inline-block ">
       <h1
         className="font-bebas 
