877393f1a9555aa0f167a3e2a22481814eba16f6
roan0001 <roan0001@stud.kea.dk>
2026-05-13 10:53:48 +0200

    icon item fixet med twitter

--- src/components/(footer components)/SocialContainer.jsx
+++ src/components/(footer components)/SocialContainer.jsx
@@ -1,4 +1,4 @@
-import TweetItem from "./TweetItem";
+import { FaTwitter } from "react-icons/fa";
 
 const tweets = [
   { id: 1, text: "It is a long established fact that a reader will be distracted by the readable...", time: "5 hours ago" },
@@ -10,7 +10,7 @@ export default function SocialContainer() {
     <div>
       <p className="mb-6 text-[0.62rem] font-bold uppercase tracking-[0.3em] text-[#e8004d]">Recent Tweets</p>
       {tweets.map((tweet) => (
-        <TweetItem key={tweet.id} text={tweet.text} time={tweet.time} />
+        <FaTwitter key={tweet.id} text={tweet.text} time={tweet.time} />
       ))}
     </div>
   );
