ae0784217ef07e35cca800c55a71508e92bbaaea
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-15 15:46:36 +0200

    rettelse

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,23 @@ import FeaturedEventsContainer from "@/components/(ForsideComponents)/FeaturedEv
 import ClubGallery from "@/components/(ForsideComponents)/ClubGallery";
 import ClubTrack from "@/components/(ForsideComponents)/ClubTrack";
 import LatestVideo from "@/components/(ForsideComponents)/LatestVideo";
+import ReviewContainer from "@/components/(ForsideComponents)/ReviewContainer";
 import NewsSubsriberContainer from "@/components/(ForsideComponents)/NewsSubscribeContainer";
 import Footer from "@/components/(footer components)/Footer";
 import Hero from "@/components/(Hero components)/Hero";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/testimonials`);
+  const testimonials = await res.json();
   return (
     <div className="bg-black">
       <Hero />
       <Navigation />
+      <WelcomeClubContainer />
       <ClubGallery />
       <LatestVideo />
+      <ReviewContainer testimonials={testimonials} />
+      <NewsSubsriberContainer />
       <Footer />
     </div>
   );
