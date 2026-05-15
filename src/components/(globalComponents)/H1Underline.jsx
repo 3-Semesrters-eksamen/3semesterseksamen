f44838ada3ca76eadae1f23d67fcb3364688a57b
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-15 12:33:19 +0200

    h1 start

---
+++ src/components/(globalComponents)/H1Underline.jsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function H1Underline() {
+  return <Image src="/bottom_line2.png" alt="Dekorativ streg" width={300} height={20} className="mt-2" />;
+}
