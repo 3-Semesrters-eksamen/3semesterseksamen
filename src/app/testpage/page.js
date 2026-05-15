f1f8f9e4ce935658e6d8dfde721fe2b644ac2046
ider2001 <ider0001@stud.kea.dk>
2026-05-15 13:51:21 +0200

    billedesection1 done

--- src/app/testpage/page.js
+++ src/app/testpage/page.js
@@ -1,12 +1,14 @@
 import Hero from "@/components/(Hero components)/Hero";
 import Navigation from "@/components/(globalComponents)/Navigation";
 import Footer from "@/components/(footer components)/Footer";
+import WelcomeClub from "@/components/(ForsideComponents)/WelcomeClubContainer";
 
 const hero = () => {
   return (
     <div>
       <Hero />
       <Navigation />
+      <WelcomeClub />
       <Footer />
     </div>
   );
