669d70ce448c81d465f40d0da426cc8e009d0af9
ider2001 <ider0001@stud.kea.dk>
2026-05-18 15:04:00 +0200

    har slettet testsiden

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
 import Navigation from "@/components/(globalComponents)/Navigation";
-import WelcomeClubContainer from "@/components/(ForsideComponents)/WelcomeClubContainer";
-import FeaturedEventsContainer from "@/components/(ForsideComponents)/FeaturedEventsContainer";
+import WelcomeClub from "@/components/(ForsideComponents)/WelcomeClubContainer";
+import EventSlider from "@/components/(ForsideComponents)/EventSlider";
 import ClubGallery from "@/components/(ForsideComponents)/ClubGallery";
 import ClubTrack from "@/components/(ForsideComponents)/ClubTrack";
 import LatestVideo from "@/components/(ForsideComponents)/LatestVideo";
@@ -12,18 +12,25 @@ import Footer from "@/components/(footer components)/Footer";
 import Hero from "@/components/(Hero components)/Hero";
 
 export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/testimonials`);
-  const testimonials = await res.json();
-  return (
-    <div className="bg-black">
-      <Hero />
-      <Navigation />
-      <WelcomeClubContainer />
-      <ClubGallery />
-      <LatestVideo />
-      <ReviewContainer testimonials={testimonials} />
-      <NewsSubsriberContainer />
-      <Footer />
-    </div>
-  );
+  {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/testimonials`);
+    const testimonials = await res.json();
+
+    const eventRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events?isFeatured=true`);
+    const events = await eventRes.json();
+
+    return (
+      <div className="bg-black">
+        <Hero />
+        <Navigation />
+        <WelcomeClub />
+        <EventSlider events={events} />
+        <ClubGallery />
+        <LatestVideo />
+        <ReviewContainer testimonials={testimonials} />
+        <NewsSubsriberContainer />
+        <Footer />
+      </div>
+    );
+  }
 }
