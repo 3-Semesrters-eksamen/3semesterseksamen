2f1afd3dde4ad8c447b3501b820dff1455e8fe97
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-18 13:38:24 +0200

    h1 rettelser

--- src/components/(globalComponents)/H1.jsx
+++ src/components/(globalComponents)/H1.jsx
@@ -3,8 +3,12 @@ import H1Underline from "./H1Underline";
 export default function H1({ children, className = "" }) {
   return (
     <div className="inline-block">
-      <h1 className={`font-semibold ${className}`}>{children}</h1>
-      <H1Underline />
+      <div>
+        <h1 className={`tracking-wide ${className}`}>{children}</h1>
+      </div>
+      <div>
+        <H1Underline />
+      </div>
     </div>
   );
 }
