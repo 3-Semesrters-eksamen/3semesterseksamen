7bcca13f1c1a6f368dc938bb505d11c70443133b
ider2001 <ider0001@stud.kea.dk>
2026-05-19 13:35:12 +0200

    font er sat ind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ubuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-ubuntu",
   subsets: ["latin"],
 });
 
@@ -18,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en" className={`${ubuntu.variable} antialiased`}>
       <body className="flex flex-col">{children}</body>
     </html>
   );
