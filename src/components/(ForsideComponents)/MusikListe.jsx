a9d4aaeaffe45efabe94cb5d14f5d90c76048fc6
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-23 17:05:43 +0200

    audio player næsten færdig

--- src/components/(ForsideComponents)/MusikListe.jsx
+++ src/components/(ForsideComponents)/MusikListe.jsx
@@ -1,5 +1,14 @@
-const MusikListe = () => {
-  return <div></div>;
-};
+"use client";
 
-export default MusikListe;
+export default function MusikListe({ tracks, onSelect }) {
+  return (
+    <div className="flex flex-row flex-wrap justify-center p-4 border ">
+      {tracks.map((track, i) => (
+        <div key={i} className="flex flex-col items-center cursor-pointer w-48" onClick={() => onSelect(track)}>
+          <img src={track.image} className="w-44 h-44 object-cover shadow" />
+          <span className="text-center mt-2">{track.title}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
