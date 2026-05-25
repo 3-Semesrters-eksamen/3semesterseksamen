4365313f97b33588958bf91771f6bf5bea9bced3
ider2001 <ider0001@stud.kea.dk>
2026-05-25 12:51:28 +0200

    responsivitet semidone

--- src/components/(ForsideComponents)/WelcomeClubContainer.jsx
+++ src/components/(ForsideComponents)/WelcomeClubContainer.jsx
@@ -4,9 +4,9 @@ import H1 from "@/components/(globalComponents)/H1";
 
 export default function WelcomeClub() {
   return (
-    <div className="flex flex-col items-center mt-10">
+    <div className="flex flex-col items-center section-base">
       <H1 className="text-white text-xl mb-6">WELCOME IN NIGHTCLUB</H1>
-      <div className="flex flex-col md:flex-row gap-4 p-4 w-full max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 container-base">
         <WelcomeClubCard image="/contentImg/thumb1.jpg" title="Welcome to Our Club" text="Discover the best club experience!" />
         <WelcomeClubCard image="/contentImg/reastaurant_1.jpg" title="Our Restaurant" text="Enjoy delicious food and drinks!" />
         <WelcomeClubCard image="/contentImg/thumb2.jpg" title="Join Us" text="Become a member and get exclusive benefits!" />
