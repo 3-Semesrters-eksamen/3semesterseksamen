60998cea099e947592a6ade9e7b88ffacd8bd6ef
mgreen1999 <mari90tc@stud.kea.dk>
2026-05-26 11:50:33 +0200

    welcomecard næsten færdig

--- src/components/(ForsideComponents)/WelcomeClubContainer.jsx
+++ src/components/(ForsideComponents)/WelcomeClubContainer.jsx
@@ -1,14 +1,16 @@
 "use client";
 import WelcomeClubCard from "@/components/(ForsideComponents)/WelcomeClubCard";
+import { PiCheersFill } from "react-icons/pi";
+import { RiServiceBellLine } from "react-icons/ri";
 
 export default function WelcomeClub() {
   return (
     <div className="flex flex-col items-center py-16 w-full">
-      <h2 className="text-white text-2xl font-bold tracking-widest mb-6">WELCOME IN NIGHTCLUB</h2>
+      <h2 className="text-white text-2xl font-bold tracking-widest mb-6 pb-8">WELCOME IN NIGHTCLUB</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-[1200px] px-4 mx-auto">
-        <WelcomeClubCard image="/contentImg/thumb1.jpg" title="Welcome to Our Club" text="Discover the best club experience!" />
-        <WelcomeClubCard image="/contentImg/reastaurant_1.jpg" title="Our Restaurant" text="Enjoy delicious food and drinks!" />
-        <WelcomeClubCard image="/contentImg/thumb2.jpg" title="Join Us" text="Become a member and get exclusive benefits!" />
+        <WelcomeClubCard image="/contentImg/thumb1.jpg" title="Welcome to Our Club" text="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable." iconImage="/icon/favicon.png" />
+        <WelcomeClubCard image="/contentImg/reastaurant_1.jpg" title="Our Restaurant" text="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout." icon={RiServiceBellLine} />
+        <WelcomeClubCard image="/contentImg/thumb2.jpg" title="Join Us" text="Contrary to popular belief, Lorem Ipsum is not simply random text." icon={PiCheersFill} />
       </div>
     </div>
   );
