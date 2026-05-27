6c8d04b0b334ebd075cc7facb63a67375819b491
ider2001 <ider0001@stud.kea.dk>
2026-05-27 18:11:32 +0200

    navigation af small logo done

--- src/components/(globalComponents)/SmallLogo.jsx
+++ src/components/(globalComponents)/SmallLogo.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SmallLogo({ className = "" }) {
-  return <Image src="/icon/Smalllogo.svg" alt="Small Logo" width={200} height={200} className={`object-contain ${className}`} />;
+  return (
+    <Link href="/">
+      <Image src="/icon/Smalllogo.svg" alt="Small Logo" width={200} height={200} className={`object-contain ${className}`} />
+    </Link>
+  );
 }
